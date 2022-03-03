0ea55983a15846e4cc946e30f05db3740a27bdf4
Jihoon Kim <kzc123@naver.com>
2022-03-03 23:47:45 +0900

    16953 greedy

--- solved/16953.py
+++ solved/16953.py
@@ -1,17 +1,32 @@
 a, b = [int(x) for x in input().split()]
+count = 0
+while True:
+    count += 1
+    if b == a:
+        break
+    if b < a:
+        count = -1
+        break
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
+    else:
+        count = -1
+        break
+print(count)
+
+# def dfs(current, count):
+#     global a, b
+#     if current == b:
+#         return count
+#     if current > b:
+#         return 123456789
+#     return min(dfs(current * 2, count + 1), dfs(current * 10 + 1, count + 1))
 
 
-def dfs(current, count):
-    global a, b
-    if current == b:
-        return count
-    if current > b:
-        return 123456789
-    return min(dfs(current * 2, count + 1), dfs(current * 10 + 1, count + 1))
-
-
-ans = dfs(a, 0) + 1
-if ans >= 123456789:
-    ans = -1
-print(ans)
+# ans = dfs(a, 0) + 1
+# if ans >= 123456789:
+#     ans = -1
+# print(ans)
 
