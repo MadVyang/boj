309a86d4ec432645d9291b0b577a2b86c59bd475
Jihoon Kim <kzc123@naver.com>
2022-03-11 23:22:43 +0900

    10886 잔디

---
+++ solved/10886.py
@@ -0,0 +1,11 @@
+n = int(input())
+ans = 0
+for i in range(n):
+    if int(input()) == 1:
+        ans += 1
+    else:
+        ans -= 1
+if ans > 0:
+    print('Junhee is cute!')
+else:
+    print('Junhee is not cute!')
