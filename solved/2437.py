4fff6a31e40f17f504e72ce5f4ca376ae5d35c43
Jihoon Kim <kzc123@naver.com>
2022-03-20 00:21:31 +0900

    2437 불필요한 예외처리

--- solved/2437.py
+++ solved/2437.py
@@ -3,13 +3,10 @@ arr = [int(x) for x in input().split()]
 arr.sort()
 
 current = 0
-ans = 0
 for i in range(n):
     if arr[i] > current+1:
-        ans =current+1
+        ans = current+1
         break
     else:
         current += arr[i]
-if ans==0:
-    ans = sum(arr)+1
-print(ans)
+print(current+1)
