9cb5070bee280652ca9c212f4a0df4fd8a760310
Jihoon Kim <kzc123@naver.com>
2022-05-06 21:49:18 +0900

    .

--- solved/15999.py
+++ solved/15999.py
@@ -4,7 +4,7 @@ import sys
 n, m = [int(x) for x in input().split()]
 arr = [sys.stdin.readline().strip() for i in range(n)]
 
-ans = 1
+ans = 0
 for i in range(n):
     for j in range(m):
         cur = arr[i][j]
@@ -16,7 +16,21 @@ for i in range(n):
             continue
         if j+1 < m and arr[i][j+1] != cur:
             continue
-        ans *= 2
-        ans %= 1000000007
+        ans += 1
 
-print(ans)
+
+def pw2(i):
+    if i == 1:
+        return 2
+    if i == 0:
+        return 1
+    r = i % 2
+    rr = pw2(i//2)
+    rr = rr*rr
+    if r == 1:
+        rr *= 2
+    return rr % 1000000007
+
+
+# print(ans)
+print(pw2(ans))
