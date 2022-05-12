c8a2f46c4361bfd3e4ab895b66de95fc0a9ff98b
Jihoon Kim <kzc123@naver.com>
2022-05-12 23:34:13 +0900

    .

---
+++ solved/5086.py
@@ -0,0 +1,10 @@
+while True:
+    a, b = [int(x) for x in input().split()]
+    if a == b == 0:
+        break
+    if b % a == 0:
+        print('factor')
+    elif a % b == 0:
+        print('multiple')
+    else:
+        print('neither')
