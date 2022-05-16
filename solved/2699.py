a485101577d2f1d5719d01099f09b240aef33d5d
Jihoon Kim <kzc123@naver.com>
2022-05-16 13:38:52 +0900

    컨벡스헐

--- solved/2699.py
+++ solved/2699.py
@@ -18,52 +18,59 @@ def ccw(x, y, z):
         return 0
 
 
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append([int(x) for x in sys.stdin.readline().strip().split()])
-
-
-def comp2(v):
-    return v[0]*100000+v[1]
-
-
-arr.sort(key=comp2)
-first = arr[0]
-rm = arr[1:]
-
-
-def comp(v):
-    return math.atan2(v[1]-first[1], v[0]-first[0])
-
-
 def dist(a, b):
     return (a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1])
 
 
-rm.sort(key=comp)
-i = 0
-while i < len(rm)-1:
-    # print(comp(rm[i]), comp(rm[i+1]), comp(rm[i]) == comp(rm[i+1]))
-    if comp(rm[i]) == comp(rm[i+1]):
-        if dist(first, rm[i]) > dist(first, rm[i+1]):
-            rm.pop(i+1)
+def comp(v):
+    return -math.atan2(v[1]-first[1], v[0]-first[0])
+
+
+def comp2(v):
+    return v[1]*100000-v[0]
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = []
+    nn = n//5
+    if n % 5 != 0:
+        nn += 1
+    for i in range(nn):
+        tmp = [int(x) for x in sys.stdin.readline().strip().split()]
+        for j in range(0, len(tmp), 2):
+            arr.append((tmp[j], tmp[j+1]))
+
+    arr.sort(key=comp2)
+    first = arr[-1]
+    rm = arr[:-1]
+
+    rm.sort(key=comp)
+    i = 0
+    while i < len(rm)-1:
+        # print(comp(rm[i]), comp(rm[i+1]), comp(rm[i]) == comp(rm[i+1]))
+        if comp(rm[i]) == comp(rm[i+1]):
+            if dist(first, rm[i]) > dist(first, rm[i+1]):
+                rm.pop(i+1)
+            else:
+                rm.pop(i)
         else:
-            rm.pop(i)
-    else:
-        i += 1
-# print(rm)
-q = [first, rm[0]]
+            i += 1
+    # print(rm)
+    q = [first, rm[0]]
 
-i = 1
-while i < len(rm):
-    c = ccw(q[-2], q[-1], rm[i])
-    # print(q[-2], q[-1], rm[i], c)
-    if c == 1:
-        q.append(rm[i])
-        i += 1
-    else:
-        q.pop()
+    i = 1
+    while i < len(rm):
+        # print(q)
+        c = ccw(q[-2], q[-1], rm[i])
+        # print(q[-2], q[-1], rm[i], c)
+        if c == -1:
+            q.append(rm[i])
+            i += 1
+        else:
+            q.pop()
 
-# print(q)
-print(len(q))
+    print(len(q))
+    for i in q:
+        print(i[0], i[1])
