bef82984067ea19aae5387d7e6bdfefa2e2abea8
Jihoon Kim <kzc123@naver.com>
2022-05-18 23:59:03 +0900

    dijk

--- solved/13549.py
+++ solved/13549.py
@@ -1,24 +1,20 @@
-from collections import deque
+import heapq
 import math
 
 
 n, k = map(int, input().split())
-q = deque([(0, n)])
+q = [(0, n)]
 vis = [math.inf]*200002
-vis[n] = 0
+vis[n]=0
 while q:
-    t, i = q.pop()
+    t, i = heapq.heappop(q)
     if i == k:
         break
-
     nexts = [(t+1, i+1), (t+1, i-1), (t, i*2)]
     for tt, ii in nexts:
-        if ii > 200000 or ii < 0 or vis[ii] <= tt:
+        if ii > 200000 or ii < 0 or vis[ii]<=tt:
             continue
         vis[ii] = tt
-        if tt == t:
-            q.append((tt, ii))
-        else:
-            q.appendleft((tt, ii))
+        heapq.heappush(q, (tt, ii))
 
 print(t)
