fdc429a577651a7b64f8d933757b1651f519572a
Jihoon Kim <kzc123@naver.com>
2022-09-20 22:07:33 +0900

    응 맞았어

--- solved/2038.py
+++ solved/2038.py
@@ -1,11 +1,12 @@
 n = int(input())
-arr = [0]*11111111
-arr[1] = 1
-cur = 1
-sum = 1
+arr = [0, 1, 2, 2]
+cur = 2
+sum = 3
 while sum < n:
     cur += 1
-    arr[cur] = 1+arr[cur-arr[arr[cur-1]]]
+    if len(arr) <= 11111111:
+        arr += [cur]*arr[cur]
     sum += arr[cur]
-
+if n == 1:
+    cur = 1
 print(cur)
