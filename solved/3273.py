1fa02ba295966fafc1d15b1e4b86d211e9f8d236
Jihoon Kim <kzc123@naver.com>
2022-11-25 23:19:06 +0900

    .

---
+++ solved/3273.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = [int(x) for x in input().split()]
+s = set(arr)
+check = set()
+x = int(input())
+
+ans = 0
+for i in range(n):
+    if arr[i] in check:
+        continue
+    if x-arr[i]!=arr[i] and x-arr[i] in s and x-arr[i] not in check:
+        check.add(arr[i])
+        check.add(x-arr[i])
+        ans += 1
+        print(arr[i])
+print(ans)
